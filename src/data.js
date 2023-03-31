c2e96b6713b8d158293103d9bb2b26abab0d3756
AashishKumar Yadav <yadavaashish2709@gmail.com>
2023-03-31 18:59:48 +0530

    :tada:complete

---
+++ src/data.js
@@ -0,0 +1,15 @@
+import { IoGameController, IoMusicalNote } from "react-icons/io5";
+import {FaSmileWink, FaFilm, FaQq} from "react-icons/fa";
+import {GiAngelOutfit} from "react-icons/gi";
+import {MdEmojiNature} from "react-icons/md";
+
+export const categories =[
+    {id: 1, name: "Games", iconSrc: <IoGameController fontSize={30} />},
+    {id: 1, name: "Funny", iconSrc: <FaSmileWink fontSize={30} />},
+    {id: 1, name: "Stories", iconSrc: <FaQq fontSize={30} />},
+    {id: 1, name: "Movies", iconSrc: <FaFilm fontSize={30} />},
+    {id: 1, name: "Anime", iconSrc: <GiAngelOutfit fontSize={30} />},
+    {id: 1, name: "Music", iconSrc: <IoMusicalNote fontSize={30} />},
+    {id: 1, name: "Nature", iconSrc: <MdEmojiNature fontSize={30} />},
+
+];
